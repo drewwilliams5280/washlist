3743192831e4995a13dc40f489528be91ba315a4
Drew Williams <drewwilliams5280@gmail.com>
2020-12-10 23:14:08 -0700

    Finishes create method in list controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,6 +4,20 @@ class ListController < ApplicationController
   end
 
   def create
-    redirect_to '/dashboard'
+    list = List.new(list_params)
+    if list.save
+      flash[:success] = "Your WashList has been created!"
+      session[:list_id] = list.id
+      redirect_to '/dashboard'
+    else
+      flash[:error] = list.errors.full_messages.uniq.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.permit(:name, :password, :password_confirmation)
   end
 end
