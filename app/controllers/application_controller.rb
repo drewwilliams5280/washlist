f33aea1577c07770fbc0b6cc340b424063dd4575
Drew Williams <drewwilliams5280@gmail.com>
2020-12-10 23:33:14 -0700

    Dream driven helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_list
+
+  def current_list
+    @current_list ||= List.find(session[:list_id]) if session[:list_id]
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @list = current_list
   end
 end
