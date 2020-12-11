3b5019d87fc52790690d52b2a541dcb8b61891f0
Drew Williams <drewwilliams5280@gmail.com>
2020-12-10 22:55:40 -0700

    Adds form to new list view

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,5 @@
 class ListController < ApplicationController
   def new
-    @truck_types = Truck.unit_types.keys
+    @unit_types = Truck.unit_types.keys
   end
 end
---
+++ spec/features/list/new_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "As a user" do
+  it "can fill in form for creating a wash list" do
+    visit '/list/new'
+    fill_in :name, with: "Amazing list"
+    fill_in :password, with: "123"
+    fill_in :password_confirmation, with: "123"
+    click_on "Submit"
+    expect(current_path).to eq('/dashboard')
+  end
+end
+
+# <%= form_with method: :post, url: '/list' do |f| %>
+#   <%= f.label :name %><br>
+#   <%= f.text_field :name, required: true %><br>
+#   <%= f.select :unit_type, @unit_types %><br><br>
+#   <%= f.submit "Create WashList" %>
+# <% end %>
+# Putting here so I don't forget
